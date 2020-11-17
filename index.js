bac4e9d52c7a7755e5b7a01117486e047ab5c2a5
crrojas88 <carlorrojas@gmail.com>
2020-11-16 22:54:03 -0800

    Done.

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ main.remove();
 
 const newHeader = document.createElement('h1');
 newHeader.id = "victory";
-newHeader.innerHTML = "YOUR-NAME is the champion";
+newHeader.innerHTML = "Rod is the champion";
